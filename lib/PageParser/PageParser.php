988b4121fe96c7e85b2c6b74db32c4526715a97f
ron.ongjoco@gmail.com <ron.ongjoco@gmail.com>
2015-10-04 17:35:38 -0700

    adding database class. adding some ideas to the parser and page parser classes

---
+++ lib/PageParser/PageParser.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * PageParser class will be the main class for parsing individual listings
+ *
+ * This class will be extended only, will not be instantiated
+ *
+ */
+
+class PageParser {
+
+
+
+}
---
+++ lib/Parser/Parser.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Parser class will be the main class for the main forum listing
+ * will grab links from the listing and call PageParser to parse
+ * individual listings
+ *
+ * This class will be extended only, will not be instantiated
+ *
+ */
+
+class Parser {
+
+
+
+}
